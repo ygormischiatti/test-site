4808ee3573307f1f9100a56fac6ee728c2d0b927
Ygor Mischiatti <ygor.mischiati@gmail.com>
2020-02-10 09:08:36 -0300

    Adição de mensagem personalizada

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+//Image switcher
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() { 
@@ -7,4 +8,29 @@ myImage.onclick = function() {
     } else {
         myImage.setAttribute('src','images/cybersecurity.png');
     }
+}
+
+//Personalized wellcome message
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Por favor, digite o seu nome.');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Cybersecurity é legal, ' + myName + '!';
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Cybersecurity é legal, ' + storedName + '!';
+}
+
+myButton.onclick = function() {
+    setUserName();
 }
